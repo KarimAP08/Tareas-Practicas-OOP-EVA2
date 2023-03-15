1871e98f8e1fab7067985dd41981b64777f17511
KarimAP08 <124391455+KarimAP08@users.noreply.github.com>
2023-03-15 12:23:29 -0600

    Add files via upload

---
+++ EVA2_9_SUPER_2/src/main/java/com/mycompany/eva2_9_super_2/Docentes.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.eva2_9_super_2;
+
+/**
+ *
+ * @author KarimAP
+ */
+//CLASE DERIVADA extends CLASE BASE
+//CLASE HIJO extends clase PADRE
+//SUBCLASE extends SUPERCLASE
+public class Docentes extends Persona {
+
+    private String plaza;
+
+    public Docentes() {
+        super();
+        this.plaza = "----";
+    }
+
+    public Docentes(String plaza, String nombre, String Apellido, int edad) {
+        super(nombre, Apellido, edad);
+        this.plaza = plaza;
+    }
+
+    public String getPlaza() {
+        return plaza;
+    }
+
+    public void setPlaza(String plaza) {
+        this.plaza = plaza;
+    }
+    
+    public void ImprimirDatos(){
+        super.ImprimirDatos();
+        System.out.println("Plaza: " +plaza);
+    }
+
+}
---
+++ EVA2_9_SUPER_2/src/main/java/com/mycompany/eva2_9_super_2/EVA2_9_SUPER_2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.eva2_9_super_2;
+
+/**
+ *
+ * @author KarimAP
+ */
+public class EVA2_9_SUPER_2 {
+
+    public static void main(String[] args) {
+        Estudiante estu = new Estudiante("2344", "Mario", "Alberto", 41);
+        estu.ImprimirDatos();
+
+        Docentes doc = new Docentes();
+        doc.ImprimirDatos();
+    }
+}
